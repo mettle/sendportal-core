9f0e662a23cdfadad26e23da6a12633be00faa3e
Aivis Silins <aivis.silins@gmail.com>
2020-03-26 08:29:36 +0000

    cleanup

--- src/SendportalBaseServiceProvider.php
+++ src/SendportalBaseServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace Sendportal\Base;
 
-use Collective\Html\FormFacade;
-use Collective\Html\HtmlFacade;
 use Collective\Html\HtmlServiceProvider;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Route;
@@ -11,8 +9,6 @@ use Illuminate\Support\ServiceProvider;
 use Sendportal\Base\Console\Commands\CampaignDispatchCommand;
 use Sendportal\Base\Console\Commands\GenerateTestSubscribers;
 use Sendportal\Base\Console\Commands\SetupProduction;
-use Sendportal\Base\Http\Middleware\OwnsCurrentTeam;
-use Sendportal\Base\Http\Middleware\VerifyUserOnTeam;
 use Sendportal\Base\Providers\EventServiceProvider;
 use Sendportal\Base\Providers\FormServiceProvider;
 use Sendportal\Base\Providers\SendportalAppServiceProvider;
@@ -57,8 +53,6 @@ class SendportalBaseServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'sendportal');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
-
-
         Route::group([
             'namespace' => 'Sendportal\Base\Http\Controllers'
         ], function () {
@@ -73,8 +67,8 @@ class SendportalBaseServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(EventServiceProvider::class);
         $this->app->register(SendportalAppServiceProvider::class);
+        $this->app->register(EventServiceProvider::class);
         $this->app->register(HtmlServiceProvider::class);
         $this->app->register(FormServiceProvider::class);
 

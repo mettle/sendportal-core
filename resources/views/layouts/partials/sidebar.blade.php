25a5b467efb279d544fac03ba6508fe50d74769b
David Field <david@mettle.io>
2020-05-25 11:39:59 +0100

    wip

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -35,7 +35,7 @@
         @if ( auth()->user()->ownsCurrentWorkspace())
             <li class="nav-item {{ request()->is('users*') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('sendportal.users.index') }}">
-                    <i class="fas fa-cog mr-2"></i><span>{{ __('Manage Users') }}</span>
+                    <i class="fas fa-users mr-2"></i><span>{{ __('Manage Users') }}</span>
                 </a>
             </li>
         @endif

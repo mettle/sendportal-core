7fa57a62c3867bccbc89a33e51574710533e4f36
David Field <david@mettle.io>
2020-05-27 09:45:26 +0100

    wip

--- src/Listeners/MessageDispatchHandler.php
+++ src/Listeners/MessageDispatchHandler.php
@@ -1,35 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sendportal\Base\Listeners;
 
+use Exception;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Sendportal\Base\Events\MessageDispatchEvent;
 use Sendportal\Base\Services\Messages\DispatchMessage;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class MessageDispatchHandler implements ShouldQueue
 {
-    /**
-     * @var string
-     */
-    public $queue = 'sendportal-message-dispatch';
+    /** @var string */
+    public $queue;
 
-    /**
-     * @var DispatchMessage
-     */
+    /** @var DispatchMessage */
     protected $dispatchMessage;
 
     public function __construct(DispatchMessage $dispatchMessage)
     {
         $this->dispatchMessage = $dispatchMessage;
+        $this->queue = config('sendportal.queue.message-dispatch');
     }
 
     /**
-     * Handle the event.
-     *
-     * @param MessageDispatchEvent $event
-     * @throws \Exception
+     * @throws Exception
      */
-    public function handle(MessageDispatchEvent $event)
+    public function handle(MessageDispatchEvent $event): void
     {
         $this->dispatchMessage->handle($event->message);
     }

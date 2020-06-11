b48cd8b7951685560dbedd470e0c930b3dc1af0c
Will Selby <will@mettle.io>
2020-06-11 13:56:23 +0100

    Bugfix - Unique Validation in PostgreSQL (#19)
    * Fix unique validation in Postgres
    
    * wip
    
    * wip
    
    * Inline the subscriber unique email rule.
    
    Co-authored-by: Joshua Franks <joshua@elfradev.com>
    Co-authored-by: David Field <david@mettle.io>

--- src/Http/Requests/SubscriberRequest.php
+++ src/Http/Requests/SubscriberRequest.php
@@ -2,33 +2,27 @@
 
 namespace Sendportal\Base\Http\Requests;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
+/**
+ * @property-read string $subscriber
+ */
 class SubscriberRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
             'email' => [
                 'required',
                 'email',
                 'max:255',
-                'unique:subscribers,email,' . $this->subscriber . ',id,workspace_id, ' . auth()->user()->currentWorkspace()->id
+                Rule::unique('subscribers', 'email')
+                    ->ignore($this->subscriber, 'id')
+                    ->where(static function (Builder $query) {
+                        $query->where('workspace_id', auth()->user()->currentWorkspace()->id);
+                    })
             ],
             'first_name' => [
                 'max:255',

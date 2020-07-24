25bef6b04a3817f83fec63cd4e4a430407e563c1
will <will@mettle.io>
2020-07-24 12:37:28 +0100

    Prevent updating a campaign if the status is not draft

--- tests/Feature/API/CampaignsControllerTest.php
+++ tests/Feature/API/CampaignsControllerTest.php
@@ -7,6 +7,8 @@ namespace Tests\Feature\API;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Arr;
+use Illuminate\Validation\ValidationException;
+use Sendportal\Base\Models\Campaign;
 use Tests\TestCase;
 
 class CampaignsControllerTest extends TestCase
@@ -83,15 +85,50 @@ class CampaignsControllerTest extends TestCase
 
         $response = $this->post($route, array_merge($request, ['api_token' => $workspace->owner->api_token]));
 
-        $data = Arr::except($request, 'recipients');
-
         $response->assertStatus(201);
-        $this->assertDatabaseHas('campaigns', $data);
-        $response->assertJson(['data' => $data]);
+        $this->assertDatabaseHas('campaigns', $request);
+        $response->assertJson(['data' => $request]);
     }
 
     /** @test */
     public function a_campaign_can_be_updated()
+    {
+        [$workspace, $emailService] = $this->createUserWithWorkspaceAndEmailService();
+
+        $campaign = factory(Campaign::class)->states('draft')->create(
+            [
+                'workspace_id' => $workspace->id,
+                'email_service_id' => $emailService->id,
+            ]
+        );
+
+        $route = route('sendportal.api.campaigns.update', [
+            'workspaceId' => $workspace->id,
+            'campaign' => $campaign->id,
+            'api_token' => $workspace->owner->api_token,
+        ]);
+
+        $request = [
+            'name' => $this->faker->word,
+            'subject' => $this->faker->word,
+            'from_name' => $this->faker->word,
+            'from_email' => $this->faker->safeEmail,
+            'email_service_id' => $emailService->id,
+            'content' => $this->faker->sentence,
+            'send_to_all' => 1,
+            'scheduled_at' => now(),
+        ];
+
+        $response = $this->put($route, $request);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('campaigns', $campaign->toArray());
+        $this->assertDatabaseHas('campaigns', $request);
+        $response->assertJson(['data' => $request]);
+    }
+
+    /** @test */
+    public function a_sent_campaign_cannot_be_updated()
     {
         [$workspace, $emailService] = $this->createUserWithWorkspaceAndEmailService();
 
@@ -114,13 +151,12 @@ class CampaignsControllerTest extends TestCase
             'scheduled_at' => now(),
         ];
 
+        $this->expectException(ValidationException::class);
+
         $response = $this->put($route, $request);
 
-        $data = Arr::except($request, 'recipients');
-
-        $response->assertStatus(200);
-        $this->assertDatabaseMissing('campaigns', $campaign->toArray());
-        $this->assertDatabaseHas('campaigns', $data);
-        $response->assertJson(['data' => $data]);
+        $this->assertDatabaseMissing('campaigns', $request);
+        $this->assertDatabaseHas('campaigns', $campaign->toArray());
+        $response->assertJson(['data' => ['message' => __('A campaign cannot be updated if its status is not draft')]]);
     }
 }

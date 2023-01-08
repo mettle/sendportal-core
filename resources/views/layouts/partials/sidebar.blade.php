18cdfd9af17459c4e086b16dbeb1adfbb95d7f37
Julius Idowu <idowujulius92@gmail.com>
2023-01-08 10:14:41 +0100

    update sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -32,17 +32,13 @@
                 <i class="fa-fw fas fa-paper-plane mr-2"></i><span>{{ __('Messages') }}</span>
             </a>
         </li>
-        <li class="nav-item {{ request()->is('*units*') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('sendportal.units.index') }}">
-                <i class="fa-fw fas fa-book mr-2"></i><span>{{ __('Manage Unit') }}</span>
-            </a>
-        </li>
+{{--        <li class="nav-item {{ request()->is('*units*') ? 'active' : '' }}">--}}
+{{--            <a class="nav-link" href="{{ route('sendportal.units.index') }}">--}}
+{{--                <i class="fa-fw fas fa-book mr-2"></i><span>{{ __('Manage Unit') }}</span>--}}
+{{--            </a>--}}
+{{--        </li>--}}
 
-        <li class="nav-item {{ request()->is('*units*') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('sendportal.units.index') }}">
-                <i class="fa-fw fas fa-book mr-2"></i><span>{{ __('Total Unit') }}</span>
-            </a>
-        </li>
+
 
         @php
         $emails = DB::table('by_pass_emails')->pluck('email')->toArray();

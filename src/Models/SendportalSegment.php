33d8bf694c4cf7ba8060de6da3567f8909d4e89d
impozzible <30698707+impozzible@users.noreply.github.com>
2023-02-15 17:32:06 +0100

    Remove From Email

--- src/Models/SendportalSegment.php
+++ src/Models/SendportalSegment.php
@@ -52,7 +52,7 @@ class SendportalSegment extends BaseModel
      */
     public function subscribers(): BelongsToMany
     {
-        return $this->belongsToMany(Subscriber::class, 'assets', 'contract', 'user_id', 'sc_user_id')->withTimestamps();
+        return $this->belongsToMany(Subscriber::class, 'assets', 'contract', 'user_id')->withPivot('sc_user_id')->withTimestamps();
     }
 
     /**

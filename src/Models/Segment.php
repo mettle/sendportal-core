12565ca54aecefa9204891e40fb276f6f1038558
impozzible <30698707+impozzible@users.noreply.github.com>
2023-03-29 19:00:32 +0100

    Add ReplyTo

--- src/Models/Segment.php
+++ src/Models/Segment.php
@@ -52,15 +52,15 @@ class Segment extends BaseModel
     /**
      * Subscribers in this tag.
      */
-    public function subscribers(): BelongsToMany
-    {
-        return $this->belongsToMany(Subscriber::class, 'assets', 'contract', 'user_id')->as('asset')->where('sendportal_subscribers.workspace_id', $this->workspace_id)
-            ->withPivot('user_id', 'sc_user_id')->withTimestamps();
-    }
-    public function getSegmentSubscribersCountAttribute(){
-        return Asset::where("contract",$this->id)->where('type','segment')->distinct('user_id')
-            ->count('user_id');
-    }
+//    public function subscribers(): BelongsToMany
+//    {
+//        return $this->belongsToMany(Subscriber::class, 'assets', 'contract', 'user_id')->as('asset')->where('sendportal_subscribers.workspace_id', $this->workspace_id)
+//            ->withPivot('user_id', 'sc_user_id')->withTimestamps();
+//    }
+//    public function getSegmentSubscribersCountAttribute(){
+//        return Asset::where("contract",$this->id)->where('type','segment')->distinct('user_id')
+//            ->count('user_id');
+//    }
 
     /**
      * Active subscribers in this tag.
